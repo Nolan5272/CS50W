d33fbdc33f8e3d25069fe46c55e5812d1c2a063e
Nolan5272 <njf5272@gmail.com>
2025-02-02 09:08:26 +0000

    add sequence.py

---
+++ L2/sequence.py
@@ -0,0 +1,18 @@
+name = "nolan"
+first = name[0]
+print(first)
+
+names = ["harold", "hermine","ron","ginny"]
+print(names)
+print(names[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+names.append("draco")
+print(names)
+
+names.sort()
+print(names)
