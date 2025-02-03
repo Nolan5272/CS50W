b0b532b244247af1f12a113352f7f355c51545ad
Nolan5272 <njf5272@gmail.com>
2025-02-03 00:28:21 +0000

    add loops.py

---
+++ L2/loops.py
@@ -0,0 +1,11 @@
+for i in range(6):
+    #same as for i in {0,1,2,3,4,5}
+    print(i)
+    
+names = ["goobert","shlonald","harold"]
+
+for name in names:
+    print(name)
+
+for character in names[0]:
+    print(character)
